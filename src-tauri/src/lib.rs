636425528b0f21b39b77418ac40ea5dddbcaae22
kangtae49 <kangtae49@gmail.com>
2025-05-06 17:53:08 +0900

    add html view

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,9 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use std::env;
 use std::sync::{RwLock};
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::fs;
+use serde::Serialize;
 use tauri::{Emitter, State};
 use tauri::AppHandle;
 
@@ -19,6 +21,15 @@ impl AppState {
     }
     pub fn get_root_folder(&self) -> String {
         self.0.read().unwrap().root_folder.clone()
+    }
+    pub fn get_root_folder_name(&self) -> String {
+        let root_folder = self.get_root_folder();
+        let folder = Path::new(&root_folder);
+        let mut folder_name = String::new();
+        if folder.exists() {
+            folder_name = folder.file_name().unwrap_or_else(|| folder.as_os_str()).to_string_lossy().to_string();
+        }
+        folder_name
     }
     pub fn set_root_folder(&self, root_folder: String) {
         self.0.write().unwrap().root_folder = root_folder;
@@ -39,33 +50,83 @@ impl LogExt for AppHandle {
 
 #[tauri::command]
 fn get_root_path(state: State<AppState>, app: AppHandle) -> String {
-    app.log("get_root_folder");
+    app.log("get_root_path");
     state.get_root_folder()
 }
 
 #[tauri::command]
+fn get_root_path_name(state: State<AppState>, app: AppHandle) -> String {
+    app.log("get_root_path_name");
+    state.get_root_folder_name()
+}
+
+#[tauri::command]
 fn set_root_path(state: State<AppState>, app: AppHandle, root_folder: String) {
-    app.log("set_root_folder");
-    if Path::new(&root_folder).exists() {
+    app.log("set_root_path");
+    let folder = Path::new(&root_folder);
+    if folder.exists() {
+        // let folder_name = folder.file_name().unwrap_or_else(|| folder.as_os_str()).to_string_lossy().to_string();
+        // state.set_root_folder(folder_name);
         state.set_root_folder(root_folder);
     }
+}
+
+#[derive(Serialize)]
+struct FileEntry {
+    name: String,
+    path: String,
+    is_dir: bool,
+}
+
+#[tauri::command]
+fn list_directory(path: String) -> Vec<FileEntry> {
+    let mut entries = Vec::new();
+    if let Ok(dir) = fs::read_dir(path) {
+        for entry in dir.flatten() {
+            let file_name = entry.file_name().into_string().unwrap_or_default();
+            let file_path = entry.path().to_string_lossy().to_string();
+            let is_dir = entry.path().is_dir();
+            if is_dir || file_name.to_lowercase().ends_with(".html") {
+                entries.push(FileEntry { name: file_name, path: file_path, is_dir });
+            }
+        }
+    }
+    entries
+}
+
+
+#[tauri::command]
+fn read_html_file(path: String) -> String {
+    fs::read_to_string(path).unwrap_or_else(|_| "<p>Not exist file</p>".to_string())
 }
 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let args: Vec<String> = env::args().collect();
-    let exe_folder = env::current_exe().unwrap()
-        .parent().unwrap()
-        .to_string_lossy().into_owned();
-    let root_folder: &String = args.get(1).unwrap_or(&exe_folder);
+    let arg_path = args.get(1).map(|path| PathBuf::from(path));
+    let default_path = env::current_exe().unwrap().parent().unwrap().to_path_buf();
+
+    let root_path = match arg_path {
+        Some(path) => {
+            if path.exists() {
+                path
+            } else {
+                default_path
+            }
+        }
+        None => default_path
+    };
+    let root_folder = root_path.to_string_lossy().into_owned();
 
     tauri::Builder::default()
-        .manage(AppState::new(root_folder))
+        .manage(AppState::new(&root_folder))
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            get_root_path,
+            get_root_path, get_root_path_name,
             set_root_path,
+            list_directory,
+            read_html_file
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/main.js
+++ src/main.js
@@ -1,11 +1,80 @@
 const { invoke: invoke_rs } = window.__TAURI__.core;
 const { listen: listen_rs } = window.__TAURI__.event;
 
+window.addEventListener("DOMContentLoaded", async () => {
+  Split(['.left-pane', '.right-pane'], {
+    gutterSize: 4,
+    sizes: [20, 80],
+  });
+
+  await loadTree();
+});
+
 listen_rs("log", (event) => {
   console.log(event.payload);
 });
 
 
+document.querySelector("#root-folder").addEventListener("click", async (e) => {
+  e.preventDefault();
+  await loadTree();
+});
+
+async function loadTree() {
+  let fileTree = document.querySelector("#file-tree");
+  fileTree.innerHTML = "";
+  const folderName = await invoke_rs("get_root_path");
+
+  await loadFiles(folderName);
+}
+
+async function loadFiles(path = ".", parentElement = null) {
+  document.querySelector("#root-folder-name").textContent = await invoke_rs("get_root_path_name");
+
+  const files = await invoke_rs("list_directory", { path });
+
+  let fileTree;
+  if (parentElement) {
+    if (parentElement.dataset.expanded === "true") {
+      parentElement.dataset.expanded = "false";
+      parentElement.removeChild(parentElement.querySelector("ul"));
+      return;
+    } else {
+      parentElement.dataset.expanded = "true";
+      fileTree = document.createElement("ul");
+      parentElement.appendChild(fileTree);
+    }
+  } else {
+    fileTree = document.querySelector("#file-tree");
+    fileTree.innerHTML = "";
+  }
+
+  files.forEach(file => {
+    const li = document.createElement("li");
+    li.style.cursor = "pointer";
+
+    if (file.is_dir) {
+      li.dataset.isFolder = "true";
+      li.dataset.expanded = "false";
+      li.innerHTML = `<i class="fa-solid fa-folder"></i> <span>${file.name}</span>`;
+      li.onclick = (event) => {
+        event.stopPropagation();
+        loadFiles(file.path, li);
+      };
+    } else {
+      li.innerHTML = `<i class="fa-solid fa-file"></i> <span>${file.name}</span>`;
+      li.onclick = async (event) => {
+        event.stopPropagation();
+        if (file.name.endsWith(".html")) {
+          const viewer = document.querySelector("#viewer");
+          viewer.innerHTML = await invoke_rs("read_html_file", { path: file.path });
+        }
+      };
+    }
+
+    fileTree.appendChild(li);
+  });
+}
 
 
 
